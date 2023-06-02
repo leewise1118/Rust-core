a4c50a32eff3d99bf6e1c5997ff77513cfa1136f
lee <1076774025@qq.com>
2023-06-02 13:08:52 +0800

    ch1

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+//! The main module and entrypoint
+//!
+//! The operating system and app also starts in this module. Kernel code starts
+//! executing from `entry.asm`, after which [`rust_main()`] is called to
+//! initialize various pieces of functionality [`clear_bss()`]. (See its source code for
+//! details.)
+//!
+//! We then call [`println!`] to display `Hello, world!`.
+#![deny(missing_docs)]
+#![deny(warnings)]
+#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
+
+use core::arch::global_asm;
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+
+global_asm!(include_str!("entry.asm"));
+
+/// the rust entry-point of os
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
---
+++ src/sbi.rs
@@ -0,0 +1,42 @@
+//新版本
+#![allow(unused)]
+use core::arch::asm;
+
+// legacy extensions: ignore fid
+const SBI_SET_TIMER: usize = 0;
+const SBI_CONSOLE_PUTCHAR: usize = 1;
+const SBI_CONSOLE_GETCHAR: usize = 2;
+const SBI_CLEAR_IPI: usize = 3;
+const SBI_SEND_IPI: usize = 4;
+const SBI_REMOTE_FENCE_I: usize = 5;
+const SBI_REMOTE_SFENCE_VMA: usize = 6;
+const SBI_REMOTE_SFENCE_VMA_ASID: usize = 7;
+
+// system reset extension
+const SRST_EXTENSION: usize = 0x53525354;
+const SBI_SHUTDOWN: usize = 0;
+
+#[inline(always)]
+fn sbi_call(eid: usize, fid: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+    let mut ret;
+    unsafe {
+        asm!(
+            "ecall",
+            inlateout("x10") arg0 => ret,
+            in("x11") arg1,
+            in("x12") arg2,
+            in("x16") fid,
+            in("x17") eid,
+        );
+    }
+    ret
+}
+
+pub fn console_putchar(c: usize) {
+    sbi_call(SBI_CONSOLE_PUTCHAR, 0, c, 0, 0);
+}
+
+pub fn shutdown() -> ! {
+    sbi_call(SRST_EXTENSION, SBI_SHUTDOWN, 0, 0, 0);
+    panic!("It should shutdown!")
+}
